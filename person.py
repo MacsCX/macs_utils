55abaed66653cc3c257d3e15f59e92198d89ba07
Macs CX <maciek-cx@protonmail.com>
2018-07-24 19:41:10 +0200

    finished create_pl_female_surname method

--- person.py
+++ person.py
@@ -7,21 +7,33 @@ pl_male_names = read_mock("PL-male_names.txt")
 pl_female_names = read_mock("PL-female_names.txt")
 pl_surnames = read_mock("PL-surnames.txt")
 
-### FINISH!
-def create_pl_female_surname(surname: str = "", second_surname: str = ""):
+
+def create_pl_female_surname(first_surname: str = "", second_surname: str = ""):
     """
     Create polish female. If input is empty, you'll get random polish surname as result
-    :param surname:
+    :param first_surname:
+    :param second_surname:
     :return:
     """
 
+    if first_surname == "":
+        while first_surname == second_surname:
+            first_surname = random.choice(pl_surnames)
+            if true_or_false(false_weight=3):
+                second_surname = random.choice(pl_surnames)
+
     def feminize(surname):
-        if surname[:-1] == ("i" or "y"):
+        if surname[-1:] == "i":
             surname = replace_char(surname, -1, "a")
 
         return surname
-    ### Just for now...
-    return surname
+
+    is_double_surname = (not second_surname == "")
+
+    if is_double_surname:
+        return "%s-%s" % (feminize(first_surname), feminize(second_surname))
+    else:
+        return feminize(first_surname)
 
 
 class Person():
