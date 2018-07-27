5d4d31b930416eaeb5905b5e10559904bb32b359
Macs CX <maciek-cx@protonmail.com>
2018-07-27 14:49:12 +0200

    mock refactor...

--- person.py
+++ person.py
@@ -1,11 +1,8 @@
 from utils import *
+from mock import *
 import os, random
 
-mock_data_path = os.path.join(get_dir_abs_path(__file__), "mock_data")
-read_mock = lambda file_name: read_csv_as_array(os.path.join(mock_data_path, file_name), has_one_column=True)
-pl_male_names = read_mock("PL-male_names.txt")
-pl_female_names = read_mock("PL-female_names.txt")
-pl_surnames = read_mock("PL-surnames.txt")
+
 
 
 def create_pl_female_surname(first_surname: str = "", second_surname: str = ""):
