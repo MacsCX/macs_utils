10242b8883725c3fd5dd498dd25b306eef16d3df
Macs CX <maciek-cx@protonmail.com>
2018-08-13 18:15:54 +0200

    create_email almost finished

--- mock.py
+++ mock.py
@@ -1,12 +1,13 @@
-from utils import *
+import utils as u
 from person import Person
 import os
+import random
 
-mock_data_path = os.path.join(get_dir_abs_path(__file__), "mock_data")
+mock_data_path = os.path.join(u.get_dir_abs_path(__file__), "mock_data")
 
 
 def read_mock_txt(file_name: str):
-    return read_csv_as_array(os.path.join(mock_data_path, file_name), has_one_column=True)
+    return u.read_csv_as_array(os.path.join(mock_data_path, file_name), has_one_column=True)
 
 
 pl_male_names = read_mock_txt("PL-male_names.txt")
@@ -18,11 +19,21 @@ names = pl_male_names + pl_female_names
 surnames = pl_surnames
 
 
-def create_email(name: str = random.choice(names), surname: str = random.choice(surnames),
-                 domain: str = random.choice(domains),
-                 user: Person = ""):
+def create_email(name: str = "", surname: str = "",
+                 domain: str = "",
+                 user: Person = None):
+    # TODO make description for doc
+    # TODO finish
 
-    name = simplify_string(name).lower()
-    surname = simplify_string(surname).lower()
+    if user is None:
+        user = Person.new_random(is_male=u.true_or_false())
+        name = user.name
+        surname = user.surname
+
+    if domain == "":
+        domain = random.choice(domains)
+
+    name = u.simplify_string(name).lower()
+    surname = u.simplify_string(surname).lower()
 
     return "{0}.{1}@{2}".format(name, surname, domain)
